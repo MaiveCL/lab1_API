3f56a52fd267752f1eeb5e6d49bc93ec9763eecc
Ton Nom <maive_@hotmail.com>
2026-01-21 10:04:34 -0500

    edit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,44 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # protection contre les envoies indésirables pour create et update
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
+
+end
+
+# 10.6.1. Extracting Partials
