f633667e42f7f6e4768e7983a24450ccaddcb015
Ton Nom <maive_@hotmail.com>
2026-01-21 19:19:21 -0500

    ritch text

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,18 @@
 class ProductsController < ApplicationController
+
+  # remplace la répétition par un before_action
+  # before_action :authenticate_user!, only: %i[ create edit update destroy ]
+    # seulement le moment de confirmer la création était bloqué (la page affichait)
+  before_action :authenticate_user!, except: %i[ index show ]
+  before_action :set_product, only: %i[ show edit update destroy ]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
+    # remplace la répétition par un before_action
   end
 
   def new
@@ -21,7 +29,8 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
+    # remplace la répétition par un before_action
   end
 
   def update
@@ -33,10 +42,19 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
   # protection contre les envoies indésirables pour create et update
   private
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: [ :name, :description ])
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
     end
 
 end
